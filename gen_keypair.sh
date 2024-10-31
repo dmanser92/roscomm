0b7a626080ce955d7d99f266fb6166a62c6ce307
Dominik Manser <dmanser92@gmail.com>
2024-10-31 12:06:28 +0100

    caddy setup

--- gen_keypair.sh
+++ gen_keypair.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 mkdir -p ./certs
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./certs/self-signed.key -out ./certs/self-signed.crt
+openssl req -x509 -nodes -newkey rsa:4096 -keyout certs/self-signed.key -out certs/self-signed.crt -days 365 \
+    -subj "/CN=localhost" \
+    -addext "subjectAltName=IP:127.0.0.1,DNS:localhost"
